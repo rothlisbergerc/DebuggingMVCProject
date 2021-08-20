070e8ed76d5d9acd07d3e45ca8f872aa6a5a5d2a
rothlisbergerc <rothlisbergercheryl@gmail.com>
2021-08-20 10:20:42 -0700

    added _context.SaveChangesAsync() to Delete HttpPost method

--- AspnetCoreWithBugs/Controllers/ProductsController.cs
+++ AspnetCoreWithBugs/Controllers/ProductsController.cs
@@ -85,6 +85,7 @@ namespace AspnetCoreWithBugs.Controllers
         {
             var product = await _context.Product.FindAsync(id);
             _context.Product.Remove(product);
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
